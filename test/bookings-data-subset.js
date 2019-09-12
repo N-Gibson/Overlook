364a4a1bdec422c81ea472cd1aa9a42443bf4f36
Noah Gibson <noah.gibson99@gmail.com>
2019-09-11 18:49:02 -0600

    Add data subsets

---
+++ test/bookings-data-subset.js
@@ -0,0 +1,27 @@
+{ bookings: [
+  {
+  userID: 4,
+  date: "2019/10/19",
+  roomNumber: 5
+  },
+  {
+  userID: 29,
+  date: "2019/10/30",
+  roomNumber: 35
+  },
+  {
+  userID: 9,
+  date: "2019/09/01",
+  roomNumber: 41
+  },
+  {
+  userID: 88,
+  date: "2019/08/28",
+  roomNumber: 13
+  },
+  {
+  userID: 52,
+  date: "2019/08/16",
+  roomNumber: 23
+  }
+] };
---
+++ test/customer-data-subset.js
@@ -0,0 +1,22 @@
+{ users: [
+  {
+  id: 1,
+  name: "Matilde Larson"
+  },
+  {
+  id: 2,
+  name: "Chadrick Lowe"
+  },
+  {
+  id: 3,
+  name: "Christian Sporer"
+  },
+  {
+  id: 4,
+  name: "Brook Christiansen"
+  },
+  {
+  id: 5,
+  name: "Noemy Little"
+  } 
+] };
